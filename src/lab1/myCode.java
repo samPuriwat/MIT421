81dc83a086f0739c5d24132df888a9e55bf71045
samPuriwat <sam.puriwat@gmail.com>
2018-06-26 15:08:19 +0700

    Test upload

---
+++ src/lab1/myCode.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class myCode {
+
+    //main
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        System.out.println("Puriwat Lertkrai");
+        System.out.println("11111111111");
+        System.out.println("MIT421");
+        System.out.println("RMUTSV");
+
+
+    }//main
+
+
+}//class
