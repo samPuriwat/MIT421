b86fa3df39f5897414919011f1a5182f9d2902e6
samPuriwat <sam.puriwat@gmail.com>
2018-07-03 16:08:34 +0700

    Lab 2 : Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,53 @@
+package lab2;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+
+        int x;
+        x = 10;
+        System.out.println(x);
+        x = 20;
+        int y = 30;
+        int t = x+y; // t = 20+30;
+
+        System.out.println(y-x);
+        t = y-x;
+        System.out.println(t);
+
+        char c = '#';
+
+        double d = 10.10;
+        System.out.println(d*x);
+
+        d = x;
+
+
+
+        String msg;
+        msg = "puriwat lertkrai";
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+
+        String msg2 = "RMUTSV";
+
+        String msg3 = msg.concat(msg2);
+        System.out.println(msg3);
+
+        String msg4 = msg +" "+ msg2;
+        System.out.println(msg4);
+
+        System.out.println("ฉันชื่อ "+msg);
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+}//clss
